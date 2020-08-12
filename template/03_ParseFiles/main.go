9e1f2c093bd94186ddaf34c04ff1e5a8f5af40c3
Ivan Petrov <ivan94hristov@gmail.com>
2020-08-12 12:58:35 +0300

    ParseFiles

---
+++ template/03_ParseFiles/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("one.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tpl, err = tpl.ParseFiles("two.gohtml", "vespa.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(tpl)
+	fmt.Printf("%T\n", tpl)
+}
